c6ed5870668d455e6f3c0921d395a2dbefe1d089
GrilloPress <grillopress@gmail.com>
2015-11-10 16:02:29 +0000

    add checklists

--- cqc-site/config.rb
+++ cqc-site/config.rb
@@ -32,6 +32,12 @@
 # Helpers
 ###
 
+helpers do
+  def is_page_selected(page)
+    current_page.url == page ? "active" : ''
+  end
+end
+
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
@@ -48,25 +54,15 @@
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+  set :relative_links, true
+  
 end
